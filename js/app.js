2e7cc167d30605c7fd54219300eff84bfefe31e0
Mishal Shashikant <mishalmsb@gmail.com>
2016-07-24 13:48:41 +0100

    intro page done

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+$(function(){
+
+  $('a[title]').tooltip();
+
+
+
+  $("#home").load("views/home.html");
+  $("#test").load("views/test.html");
+  $("#test").load("views/test.html");
+  $("#test").load("views/test.html");
+  $("#contact").load("views/contact.html");
+
+
+});
