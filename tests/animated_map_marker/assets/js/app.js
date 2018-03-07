9389925540c77d1b651ea98fd043a57fa222a0e0
Mishal Shashikant <mishalmsb@gmail.com>
2018-03-07 21:29:49 +0000

    Displaying random points on map and 2 pulse versions

--- tests/animated_map_marker/assets/js/app.js
+++ tests/animated_map_marker/assets/js/app.js
@@ -1,4 +1,16 @@
-var btn, modal, span;
+var btn,
+	modal,
+	span,
+	$mapMarkers = $('#map').find('.marker-wrapper'),
+	$markersIndexes,
+	$usedMarkersIndexes = [],
+	$nextMarkersIndex,
+	$currentMarkersIndex,
+	counter = 0;
+
+$(document).ready(function() {
+	setMapMarkers();
+});
 
 function openModal(modalName) {
 	modal = document.getElementById(modalName);
@@ -20,3 +32,43 @@ window.onclick = function(event) {
 	}
 };
 
+function generateArrayWithRange(start, end) {
+	return Array(end - start + 1)
+		.fill()
+		.map((_, idx) => start + idx);
+}
+
+function setMapMarkers() {
+	$markersIndexes = generateArrayWithRange(0, $mapMarkers.length - 1);
+	animateMap();
+}
+
+function animateMap() {
+	setMarkersIndexes();
+	$($mapMarkers[$nextMarkersIndex]).fadeIn(500);
+	if ($currentMarkersIndex != undefined) {
+		$($mapMarkers[$currentMarkersIndex]).fadeOut(1000);
+	}
+	resetMarkersIndexes();
+	setTimeout(function() {
+		animateMap();
+	}, 3000);
+}
+
+function setMarkersIndexes() {
+	$usedMarkersIndexes.push(
+		$markersIndexes.splice(
+			Math.floor(Math.random() * $markersIndexes.length),
+			1
+		)[0]
+	);
+	$nextMarkersIndex = $usedMarkersIndexes[$usedMarkersIndexes.length - 1];
+}
+
+function resetMarkersIndexes() {
+	$currentMarkersIndex = $nextMarkersIndex;
+	if ($markersIndexes.length == 0) {
+		$markersIndexes = $usedMarkersIndexes;
+		$usedMarkersIndexes = [];
+	}
+}
