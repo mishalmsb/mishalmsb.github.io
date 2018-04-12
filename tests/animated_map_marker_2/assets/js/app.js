0089555c2f4c48db56e3baeff098ce83137c4f72
mishal <mishal@vastari.com>
2018-04-12 14:37:21 +0100

    swop animated_map_marker <=> animated_map_marker_2

--- tests/animated_map_marker_2/assets/js/app.js
+++ tests/animated_map_marker_2/assets/js/app.js
@@ -1,8 +1,74 @@
-//
+var btn,
+	modal,
+	span,
+	$mapMarkers = $('#map').find('.marker-wrapper'),
+	$markersIndexes,
+	$usedMarkersIndexes = [],
+	$nextMarkersIndex,
+	$currentMarkersIndex,
+	counter = 0;
 
-$("#mark").click( function(){
-  console.log('button clicked');
-  var topPos = $('#top').val() + '%';
-  var leftPos = $('#left').val() + '%';
-  $("#marker").css({top: topPos, left: leftPos});
+$(document).ready(function() {
+	setMapMarkers();
 });
+
+function openModal(modalName) {
+	modal = document.getElementById(modalName);
+	modal.style.opacity = 1;
+	modal.style.pointerEvents = 'auto';
+	span = document.getElementsByClassName('close')[0];
+}
+
+function closeModal(modalName) {
+	modal = document.getElementById(modalName);
+	modal.style.opacity = 0;
+	modal.style.pointerEvents = 'none';
+}
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.opacity = 0;
+		modal.style.pointerEvents = 'none';
+	}
+};
+
+function generateArrayWithRange(start, end) {
+	return Array(end - start + 1)
+		.fill()
+		.map((_, idx) => start + idx);
+}
+
+function setMapMarkers() {
+	$markersIndexes = generateArrayWithRange(0, $mapMarkers.length - 1);
+	animateMap();
+}
+
+function animateMap() {
+	setMarkersIndexes();
+	$($mapMarkers[$nextMarkersIndex]).fadeIn(500);
+	if ($currentMarkersIndex != undefined) {
+		$($mapMarkers[$currentMarkersIndex]).fadeOut(1000);
+	}
+	resetMarkersIndexes();
+	setTimeout(function() {
+		animateMap();
+	}, 3000);
+}
+
+function setMarkersIndexes() {
+	$usedMarkersIndexes.push(
+		$markersIndexes.splice(
+			Math.floor(Math.random() * $markersIndexes.length),
+			1
+		)[0]
+	);
+	$nextMarkersIndex = $usedMarkersIndexes[$usedMarkersIndexes.length - 1];
+}
+
+function resetMarkersIndexes() {
+	$currentMarkersIndex = $nextMarkersIndex;
+	if ($markersIndexes.length == 0) {
+		$markersIndexes = $usedMarkersIndexes;
+		$usedMarkersIndexes = [];
+	}
+}
