30d44ae5996c72e6b28957cf86767abe71583845
mishal <mishal@vastari.com>
2018-04-12 16:03:39 +0100

    added form submit

--- tests/animated_map_marker/assets/js/app.js
+++ tests/animated_map_marker/assets/js/app.js
@@ -1,8 +1,21 @@
 //
 
-$("#mark").click( function(){
+// $("#mark").click( function(){
+//   console.log('button clicked');
+//   var topPos = $('#top').val() + '%';
+//   var leftPos = $('#left').val() + '%';
+//   $("#marker").css({top: topPos, left: leftPos});
+// });
+
+
+$("#map-form").submit(function(e){
+  e.preventDefault();
+  markPoint()
+});
+
+function markPoint() {
   console.log('button clicked');
   var topPos = $('#top').val() + '%';
   var leftPos = $('#left').val() + '%';
   $("#marker").css({top: topPos, left: leftPos});
-});
+}
